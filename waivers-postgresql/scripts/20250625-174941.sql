73e06bc248ee7178d96cb227e3e37927e3874bc6
Michael Bryzek <mbryzek@alum.mit.edu>
2025-06-25 13:49:57 -0400

    wip

--- waivers-postgresql/scripts/20250625-174941.sql
+++ waivers-postgresql/scripts/20250625-174941.sql
@@ -12,8 +12,19 @@ create table signatures(
   signed_at              timestamptz,
   pdf_url                text check(util.null_or_non_empty_trimmed_string(pdf_url)),
   ip_address             text check(util.null_or_non_empty_trimmed_string(ip_address)),
-  created_at             timestamptz not null,
-  updated_at             timestamptz not null,
+  created_at             timestamptz default now() not null,
+  updated_at             timestamptz default now() not null,
   updated_by_user_id     text not null check(util.non_empty_trimmed_string(updated_by_user_id)),
   hash_code              bigint not null
 );
+
+create index signatures_user_id_idx on signatures(user_id);
+
+create index signatures_waiver_id_idx on signatures(waiver_id);
+
+alter table signatures
+  add constraint signatures_waiver_id_fk
+  foreign key(waiver_id)
+  references public.waivers;
+
+select schema_evolution_manager.create_updated_at_trigger('public', 'signatures');
